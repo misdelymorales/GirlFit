f087f13199605d2ce5a6aeb255b645cdd5dc9181
camialexisf <camialexisflores@gmail.com>
2022-08-22 19:11:54 -0400

    commit para que me deje hacer pull

--- src/sessions.js
+++ src/sessions.js
@@ -5,11 +5,15 @@
 // export default hidden();
 
 
-// const singin= document.getElementById("container-sing-in");
-// const singup= document.getElementById("container-sign-up");
-const btnLogin= document.getElementById("btn-login").addEventListener("click", next);
+//const singin= document.getElementById("container-sing-in");
+//const singup= document.getElementById("container-sign-up");
+//const btnLogin= document.getElementById("btn-login").addEventListener("click", next);
 
-export function next(){
+/*export show(){
+const singin= document.getElementById("container-sing-in");
+const singup= document.getElementById("container-sign-up");
+}*/
+/*export function next(){
    const idSend = this.getAttribute("data-id");
    const nextPage= item.getAttribute("id");
    if(id !== idSend) {
@@ -18,8 +22,31 @@ export function next(){
     item.classList.remove("hidden");
    }
 
-}
+}*/
+
+/*export const hidden = () =>{
+    const btnlogin = document.getElementById('.sing-in-btn');
+    const btnSingUp = document.getElementById('.Sing-in-up');
+    
+    document.addEventListener('click', e => {
+        if(e.target === btnlogin) {
+          singIn.setAttribute("class", "hiddenDiv");
+          singUp.setAttribute("class", 'showDiv');
+        }   
+    console.log("hola")
+    })}
 
 
+/*export const hidden = () =>{
+const $btnlogin = document.getElementById('.sing-in-btn');
+      $btnSingUp = document.getElementById('.Sing-in-up');
+      $SingUp = document.getElementById ('sing-up');
+      $SingIn = document.getElementById ('sing-in');
 
-// const 
+
+document.addEventListener('click', e => {
+    if(e.target === $btnlogin || e.target === $btnSingUp) {
+      $SingIn.classList.toogle('active');
+      $SingUp.classList.toogle('active')
+    }   
+})}*/
