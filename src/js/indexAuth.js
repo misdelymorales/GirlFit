d71e0ef2f38631b5e9fcfd1d9d021d10fedcc9a1
luisaele <luisaele28@gmail.com>
2022-08-26 09:38:19 -0400

    arreglo de codigo

--- src/js/indexAuth.js
+++ src/js/indexAuth.js
@@ -18,7 +18,7 @@ let app;
 
 //Función de eventos
 function addEvents() {
-    document.getElementById("login-google").addEventListener("click", loginUsingGoogle);
+    //document.getElementById("login-google").addEventListener("click", loginUsingGoogle);//
     document.getElementById("btn-login").addEventListener("click", login);
     document.getElementById("btn-sing-up").addEventListener("click", register);
     document.getElementById("register-link").addEventListener("click", ()=>goTo("container-sign-up")); //evento que cambia vista de registro
@@ -83,28 +83,26 @@ function login(){
 
 //}//
 
-
-
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-   signInWithPopup(auth, provider)
-  .then((result) => {
+   //signInWithPopup(auth, provider) 
+  // const provider = new GoogleAuthProvider();
+   //const auth = getAuth();
+  // .then((result) => {
     // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
+   // const credential = GoogleAuthProvider.credentialFromResult(result);
+   // const token = credential.accessToken;
     // The signed-in user info.
-    const user = result.user;
+   // const user = result.user;
     // ...
-  }).catch((error) => {
+  //}).catch((error) => {
     // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
+    //const errorCode = error.code;
+   // const errorMessage = error.message;
     // The email of the user's account used.
-    const email = error.customData.email;
+   // const email = error.customData.email;
     // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
+    //const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
-  });
+ // });//
 
 //Configuración FIREBASE
 function initilalizeFirebase () {
