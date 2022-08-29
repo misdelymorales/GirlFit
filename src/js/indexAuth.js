c30a0fdcab345e3fc91ba17154112e3123fb72f6
luisaele <luisaele28@gmail.com>
2022-08-29 11:05:29 -0400

    cambios en css

--- src/js/indexAuth.js
+++ src/js/indexAuth.js
@@ -67,68 +67,6 @@ function login(){
   });
 }
 
-<<<<<<< HEAD
-const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    //const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-
-
-//const loginUsingGoogle = () => {
- // loginUsingGoogleWithPopup(auth, provider)
-   // .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-     // const credential = GoogleAuthProvider.credentialFromResult(result);
-    
-     // return credential;
-    //})
-    //.catch((error) => {
-      
-      //const errorCode = error.code;
-    
-     // return errorCode;
-   // });
-//};
-
- //const loginUsingGoogle = () => {
-   //loginUsingWithPopup(auth, provider) 
-   //.then((result) => {
-   //const provider = new GoogleAuthProvider();
-   //const auth = getAuth();
-   ////This gives you a Google Access Token. You can use it to access the Google API.
-   //const credential = GoogleAuthProvider.credentialFromResult(result);
-   //const token = credential.accessToken;
-   ////The signed-in user info.
-   //const user = result.user;
-    // ...
-    //}).catch((error) => {
-    ////Handle Errors here.
-    //const errorCode = error.code;
-    //const errorMessage = error.message;
-    //The email of the user's account used.
-    //const email = error.customData.email;
-    //The AuthCredential type that was used.
-    //const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  //});
-
-//}
-=======
 //Función para iniciar sesión con Google
 const signWithGoogle = () => {
   const auth = getAuth(app);
@@ -156,7 +94,6 @@ const signWithGoogle = () => {
       return errorCode;
     });
 };
->>>>>>> refs/remotes/origin/main
 
 //Configuración FIREBASE
 function initilalizeFirebase () {
