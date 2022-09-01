4b0f8abcf67327fb2d1f00add885f0bc5ffe7796
Misdely Morales <misdely.m@gmail.com>
2022-09-01 10:07:31 -0400

    probando menu desplegable

--- src/view/components/nav.js
+++ src/view/components/nav.js
@@ -3,11 +3,13 @@ import {Component} from '../component.js';
     const currentPath=  window.location.hash;
     console.log(currentPath);
     const menu=(currentPath === '#/login' || currentPath==='#/register') ? '' : `
-    <div ><img class="nav-menu-icon" src="img/Imagenes/nav.png">
+    <div >
+        <div class="nav-menu-icon"><i id="menu-icon" class="fa-solid fa-circle-caret-down"></i></div>
         <ul clas="nav-links">
             <li> <a href="#">Editar Perfil </a></li>
             <li> <a href="Sign in">Cerrar Sesión </a></li>
         </ul>
+
     </div>`;
 
 
@@ -23,3 +25,4 @@ const screenWidth = window.innerWidth;
 template = ((currentPath === '#/login' || currentPath === '#/register') && screenWidth < 800 ) ? '' : template;
 
 export const nav= new Component(template, undefined, 'nav');
+
