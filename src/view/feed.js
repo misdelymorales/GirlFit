ede705db8cfeeb04e4a9b7dc8f624a41c64f0182
luisaele <luisaele28@gmail.com>
2022-09-08 22:19:47 -0300

    primeros posts

--- src/view/feed.js
+++ src/view/feed.js
@@ -1,14 +1,22 @@
 import {Component} from './component.js';
 import {nav} from './components/nav.js';
+import { createpost } from '../lib/firebase.js';
 
 const onLoad = () => {
     nav.render('nav');
+    createpost();
 }
 
 const template = `
     <div id="container-feed">
-    <h1>Feed</h1>
-    <div>
+    <input id="inputPost" type="textarea" placeholder="Cuentanos que entrenamiento vas hacer hoy...">
+    <div id="barraRosada">
+     <div id="iconos">
+         <img src="./img/iconos/pic.png" alt="fotos">
+    </div>
+    <div id="postBtn">   <button id="publishPost">Publicar</button>           
+    </div>
+</div>
 `;
 
 export const feed= new Component(template, onLoad);
